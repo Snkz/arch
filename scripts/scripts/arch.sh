6c0ce45c23080f62adec2aac0ddd9a3ae553b568
Abdi Dahir <abdi.dahir@outlook.com>
2021-04-18 22:44:25 -0700

    Update conky for new lua format

---
+++ scripts/scripts/arch.sh
@@ -0,0 +1,5 @@
+python3 /usr/bin/archey3
+while [ true ]
+do
+    sleep 200
+done
---
+++ scripts/scripts/sound.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+# file:            ~/scripts/sound.sh                                            
+# author:          Abdi Dahir - I have no website :(                           
+# modified:        May 2012                                               
+# vim: set fenc=utf-8:nu:ai:si:et:ts=4:sw=4:                      
+#                                                                             
+# Script to get sound for conky using amixer and a mono master speaker 
+#-------------------------------------------------------------------------------
+
+channel="Mono:"
+vol=`amixer get Master | grep $channel | awk '{print $4}'`
+mute=`amixer get Master | grep $channel | awk '{print $6}'`
+if [ $vol == "[0%]" ] || [ $mute == "[off]" ]
+then
+	echo "0"
+else
+	echo $vol | tr -d "[%]"
+fi
